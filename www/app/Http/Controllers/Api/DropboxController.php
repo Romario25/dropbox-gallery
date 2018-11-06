58dabec33524369e652656fe71f3f5839c6eda8d
poker <uca8@mail.ru>
2018-11-07 00:06:58 +0200

    dropbox controller

--- www/app/Http/Controllers/Api/DropboxController.php
+++ www/app/Http/Controllers/Api/DropboxController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\Api;
 
+use Dropbox\AppInfo;
+use Dropbox\WebAuthNoRedirect;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -10,6 +12,15 @@ class DropboxController extends Controller
     public function webhook(Request $request) {
 
 
+    }
+
+    public function getFiles(Request $request) {
+        $folder = 'dropbox-gallery';
+
+
+
+        $appInfo = AppInfo::loadFromJsonFile("INSERT_PATH_TO_JSON_CONFIG_PATH");
+        $webAuth = new WebAuthNoRedirect($appInfo, "PHP-Example/1.0");
     }
 
 }
